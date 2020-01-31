5978b0dc62d884d05e22c4bf7d11d8c22648f11a
Grahame Grieve <grahameg@gmail.com>
2020-01-31 19:48:58 +1100

    first commit

---
+++ update.bat
@@ -0,0 +1,46 @@
+cd C:\work\org.hl7.fhir\test-igs\hl7.fhir.us.core
+git pull
+git reset --hard
+git clean -fd
+
+cd C:\work\org.hl7.fhir\test-igs\hl7.fhir.us.ecr
+git pull
+git reset --hard
+git clean -fd
+
+cd C:\work\org.hl7.fhir\test-igs\hl7.fhir.uv.ips
+git pull
+git reset --hard
+git clean -fd
+
+cd C:\work\org.hl7.fhir\test-igs\hl7.fhir.uv.sdc
+git pull
+git reset --hard
+git clean -fd
+
+cd C:\work\org.hl7.fhir\test-igs\ihe.mhd.fhir
+git pull
+git reset --hard
+git clean -fd
+
+cd C:\work\org.hl7.fhir\test-igs\fhir.base.template
+git pull
+git reset --hard
+git clean -fd
+
+cd C:\work\org.hl7.fhir\test-igs\hl7.base.template
+git pull
+git reset --hard
+git clean -fd
+
+cd C:\work\org.hl7.fhir\test-igs\hl7.fhir.au.base
+git pull
+git reset --hard
+git clean -fd
+
+cd C:\work\org.hl7.fhir\test-igs\hl7.fhir.template
+git pull
+git reset --hard
+git clean -fd
+
+pause
---
+++ upgrade.bat
@@ -0,0 +1,34 @@
+
+set ver=1.0.53
+
+
+copy hl7.fhir.us.core\output\qa.txt records\hl7.fhir.us.core-%ver%-qa.txt
+copy hl7.fhir.us.ecr\output\qa.txt records\hl7.fhir.us.ecr-%ver%-qa.txt
+copy hl7.fhir.uv.ips\output\qa.txt records\hl7.fhir.uv.ips-%ver%-qa.txt
+copy hl7.fhir.uv.sdc\output\qa.txt records\hl7.fhir.uv.sdc-%ver%-qa.txt
+copy ihe.mhd.fhir\output\qa.txt records\ihe.mhd.fhir-%ver%-qa.txt
+copy hl7.fhir.au.base\output\qa.txt records\hl7.fhir.au.base-%ver%-qa.txt
+
+break
+
+copy hl7.fhir.us.core\output\qa.json records\hl7.fhir.us.core-%ver%-qa.json
+copy hl7.fhir.us.ecr\output\qa.json records\hl7.fhir.us.ecr-%ver%-qa.json
+copy hl7.fhir.uv.ips\output\qa.json records\hl7.fhir.uv.ips-%ver%-qa.json
+copy hl7.fhir.uv.sdc\output\qa.json records\hl7.fhir.uv.sdc-%ver%-qa.json
+copy ihe.mhd.fhir\output\qa.json records\ihe.mhd.fhir-%ver%-qa.json
+copy fhir.base.template\output\qa.json records\fhir.base.template-%ver%-qa.json
+copy hl7.base.template\output\qa.json records\hl7.base.template-%ver%-qa.json
+copy hl7.fhir.au.base\output\qa.json records\hl7.fhir.au.base-%ver%-qa.json
+copy hl7.fhir.template\output\qa.json records\hl7.fhir.template-%ver%-qa.json
+
+copy hl7.fhir.us.core\output\qa.html records\hl7.fhir.us.core-%ver%-qa.html
+copy hl7.fhir.us.ecr\output\qa.html records\hl7.fhir.us.ecr-%ver%-qa.html
+copy hl7.fhir.uv.ips\output\qa.html records\hl7.fhir.uv.ips-%ver%-qa.html
+copy hl7.fhir.uv.sdc\output\qa.html records\hl7.fhir.uv.sdc-%ver%-qa.html
+copy ihe.mhd.fhir\output\qa.html records\ihe.mhd.fhir-%ver%-qa.html
+copy fhir.base.template\output\qa.html records\fhir.base.template-%ver%-qa.html
+copy hl7.base.template\output\qa.html records\hl7.base.template-%ver%-qa.html
+copy hl7.fhir.au.base\output\qa.html records\hl7.fhir.au.base-%ver%-qa.html
+copy hl7.fhir.template\output\qa.html records\hl7.fhir.template-%ver%-qa.html
+
+
