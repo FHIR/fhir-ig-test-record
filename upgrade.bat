a1d30339b87b34c56bf805521aaca11b9cc02c71
Grahame Grieve <grahameg@gmail.com>
2020-02-02 09:09:51 +1100

    reorg after experience with first upgrade

--- upgrade.bat
+++ upgrade.bat
@@ -1,34 +1,63 @@
+REM run this batch file after a new version has been released in order to reset the test framework
 
-set ver=1.0.53
+set ver=1.0.54
+cd records 
+md v%ver%
+cd ..
+
+copy hl7.fhir.us.core\output\qa.txt records\v%ver%\hl7.fhir.us.core-qa.txt
+copy hl7.fhir.us.ecr\output\qa.txt records\v%ver%\hl7.fhir.us.ecr-qa.txt
+copy hl7.fhir.uv.ips\output\qa.txt records\v%ver%\hl7.fhir.uv.ips-qa.txt
+copy hl7.fhir.uv.sdc\output\qa.txt records\v%ver%\hl7.fhir.uv.sdc-qa.txt
+copy ihe.mhd.fhir\output\qa.txt records\v%ver%\ihe.mhd.fhir-qa.txt
+copy hl7.fhir.au.base\output\qa.txt records\v%ver%\hl7.fhir.au.base-qa.txt
+
+copy hl7.fhir.us.core\output\qa.json records\v%ver%\hl7.fhir.us.core-qa.json
+copy hl7.fhir.us.ecr\output\qa.json records\v%ver%\hl7.fhir.us.ecr-qa.json
+copy hl7.fhir.uv.ips\output\qa.json records\v%ver%\hl7.fhir.uv.ips-qa.json
+copy hl7.fhir.uv.sdc\output\qa.json records\v%ver%\hl7.fhir.uv.sdc-qa.json
+copy ihe.mhd.fhir\output\qa.json records\v%ver%\ihe.mhd.fhir-qa.json
+copy fhir.base.template\output\qa.json records\v%ver%\fhir.base.template-qa.json
+copy hl7.base.template\output\qa.json records\v%ver%\hl7.base.template-qa.json
+copy hl7.fhir.au.base\output\qa.json records\v%ver%\hl7.fhir.au.base-qa.json
+copy hl7.fhir.template\output\qa.json records\v%ver%\hl7.fhir.template-qa.json
+
+copy hl7.fhir.us.core\output\qa.html records\v%ver%\hl7.fhir.us.core-qa.html
+copy hl7.fhir.us.ecr\output\qa.html records\v%ver%\hl7.fhir.us.ecr-qa.html
+copy hl7.fhir.uv.ips\output\qa.html records\v%ver%\hl7.fhir.uv.ips-qa.html
+copy hl7.fhir.uv.sdc\output\qa.html records\v%ver%\hl7.fhir.uv.sdc-qa.html
+copy ihe.mhd.fhir\output\qa.html records\v%ver%\ihe.mhd.fhir-qa.html
+copy fhir.base.template\output\qa.html records\v%ver%\fhir.base.template-qa.html
+copy hl7.base.template\output\qa.html records\v%ver%\hl7.base.template-qa.html
+copy hl7.fhir.au.base\output\qa.html records\v%ver%\hl7.fhir.au.base-qa.html
+copy hl7.fhir.template\output\qa.html records\v%ver%\hl7.fhir.template-qa.html
 
 
-copy hl7.fhir.us.core\output\qa.txt records\hl7.fhir.us.core-%ver%-qa.txt
-copy hl7.fhir.us.ecr\output\qa.txt records\hl7.fhir.us.ecr-%ver%-qa.txt
-copy hl7.fhir.uv.ips\output\qa.txt records\hl7.fhir.uv.ips-%ver%-qa.txt
-copy hl7.fhir.uv.sdc\output\qa.txt records\hl7.fhir.uv.sdc-%ver%-qa.txt
-copy ihe.mhd.fhir\output\qa.txt records\ihe.mhd.fhir-%ver%-qa.txt
-copy hl7.fhir.au.base\output\qa.txt records\hl7.fhir.au.base-%ver%-qa.txt
+copy hl7.fhir.us.core\output\qa.txt records\hl7.fhir.us.core-qa.txt
+copy hl7.fhir.us.ecr\output\qa.txt records\hl7.fhir.us.ecr-qa.txt
+copy hl7.fhir.uv.ips\output\qa.txt records\hl7.fhir.uv.ips-qa.txt
+copy hl7.fhir.uv.sdc\output\qa.txt records\hl7.fhir.uv.sdc-qa.txt
+copy ihe.mhd.fhir\output\qa.txt records\ihe.mhd.fhir-qa.txt
+copy hl7.fhir.au.base\output\qa.txt records\hl7.fhir.au.base-qa.txt
 
-break
+copy hl7.fhir.us.core\output\qa.json records\hl7.fhir.us.core-qa.json
+copy hl7.fhir.us.ecr\output\qa.json records\hl7.fhir.us.ecr-qa.json
+copy hl7.fhir.uv.ips\output\qa.json records\hl7.fhir.uv.ips-qa.json
+copy hl7.fhir.uv.sdc\output\qa.json records\hl7.fhir.uv.sdc-qa.json
+copy ihe.mhd.fhir\output\qa.json records\ihe.mhd.fhir-qa.json
+copy fhir.base.template\output\qa.json records\fhir.base.template-qa.json
+copy hl7.base.template\output\qa.json records\hl7.base.template-qa.json
+copy hl7.fhir.au.base\output\qa.json records\hl7.fhir.au.base-qa.json
+copy hl7.fhir.template\output\qa.json records\hl7.fhir.template-qa.json
 
-copy hl7.fhir.us.core\output\qa.json records\hl7.fhir.us.core-%ver%-qa.json
-copy hl7.fhir.us.ecr\output\qa.json records\hl7.fhir.us.ecr-%ver%-qa.json
-copy hl7.fhir.uv.ips\output\qa.json records\hl7.fhir.uv.ips-%ver%-qa.json
-copy hl7.fhir.uv.sdc\output\qa.json records\hl7.fhir.uv.sdc-%ver%-qa.json
-copy ihe.mhd.fhir\output\qa.json records\ihe.mhd.fhir-%ver%-qa.json
-copy fhir.base.template\output\qa.json records\fhir.base.template-%ver%-qa.json
-copy hl7.base.template\output\qa.json records\hl7.base.template-%ver%-qa.json
-copy hl7.fhir.au.base\output\qa.json records\hl7.fhir.au.base-%ver%-qa.json
-copy hl7.fhir.template\output\qa.json records\hl7.fhir.template-%ver%-qa.json
-
-copy hl7.fhir.us.core\output\qa.html records\hl7.fhir.us.core-%ver%-qa.html
-copy hl7.fhir.us.ecr\output\qa.html records\hl7.fhir.us.ecr-%ver%-qa.html
-copy hl7.fhir.uv.ips\output\qa.html records\hl7.fhir.uv.ips-%ver%-qa.html
-copy hl7.fhir.uv.sdc\output\qa.html records\hl7.fhir.uv.sdc-%ver%-qa.html
-copy ihe.mhd.fhir\output\qa.html records\ihe.mhd.fhir-%ver%-qa.html
-copy fhir.base.template\output\qa.html records\fhir.base.template-%ver%-qa.html
-copy hl7.base.template\output\qa.html records\hl7.base.template-%ver%-qa.html
-copy hl7.fhir.au.base\output\qa.html records\hl7.fhir.au.base-%ver%-qa.html
-copy hl7.fhir.template\output\qa.html records\hl7.fhir.template-%ver%-qa.html
+copy hl7.fhir.us.core\output\qa.html records\hl7.fhir.us.core-qa.html
+copy hl7.fhir.us.ecr\output\qa.html records\hl7.fhir.us.ecr-qa.html
+copy hl7.fhir.uv.ips\output\qa.html records\hl7.fhir.uv.ips-qa.html
+copy hl7.fhir.uv.sdc\output\qa.html records\hl7.fhir.uv.sdc-qa.html
+copy ihe.mhd.fhir\output\qa.html records\ihe.mhd.fhir-qa.html
+copy fhir.base.template\output\qa.html records\fhir.base.template-qa.html
+copy hl7.base.template\output\qa.html records\hl7.base.template-qa.html
+copy hl7.fhir.au.base\output\qa.html records\hl7.fhir.au.base-qa.html
+copy hl7.fhir.template\output\qa.html records\hl7.fhir.template-qa.html
 
 
